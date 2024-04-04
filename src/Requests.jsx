06586b13368baacdf1babcd3de35c0bf3554de19
Jabir Hussain A T <138150152+JabirHussain-AT@users.noreply.github.com>
2024-04-03 20:57:53 -0700

    Update Requests.jsx

--- src/Requests.jsx
+++ src/Requests.jsx
@@ -1,4 +1,12 @@
-const key = '083a93d7a5e4d6be928679d29ebba399'
+// const key = '083a93d7a5e4d6be928679d29ebba399'
+// const requests = {
+//     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+//     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+//     requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+//     requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+//     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+//   };
+const key = '5a70268c493bd7f8acd56844a5d4c40b'
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
@@ -6,5 +14,4 @@ const requests = {
     requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
   };
-
   export default requests
